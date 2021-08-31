8b36bbe44ccc845d6cb7fbcd0c5f3e8494e7d940
cahangeorge <cahangeorge@gmail.com>
2021-08-31 19:29:47 +0300

    update contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -24,13 +24,13 @@ export default function Contact() {
                 <div className='bar-h2' data-aos='zoom-in'></div>
                 <p data-aos='zoom-in'>Have a question or want to work together?</p>
             </div>
-            <form onSubmit={sendEmail} className='d-flex flex-column align-items-center justify-content-center pb-5'>
-                <input type="text" name='name' placeholder='Your Name' className='shadow p-2 m-3' data-aos='zoom-in' />
-                <input type="email" name='email' placeholder='Your Email' className='shadow p-2 m-3' data-aos='zoom-in' />
-                <textarea name="message" cols="30" rows="10" placeholder='Your Message' className='shadow p-2 m-3' data-aos='zoom-in' />
-                <input type="submit" value='Submit' className='submit shadow p-2 m-3' data-aos='zoom-in' />
+            <form onSubmit={sendEmail} className='d-flex flex-column align-items-center justify-content-center pb-5' data-aos='zoom-in'>
+                <input type="text" name='name' placeholder='Your Name' className='shadow p-2 m-3' />
+                <input type="email" name='email' placeholder='Your Email' className='shadow p-2 m-3' />
+                <textarea name="message" cols="30" rows="10" placeholder='Your Message' className='shadow p-2 m-3' />
+                <input type="submit" value='Submit' className='submit shadow p-2 m-3' />
             </form>
-            <div className='copyright mb-0 px-3 py-4 border-top border-2 w-100 text-center' data-aos='zoom-in'>Cahan George &copy;2021</div>
+            <div className='copyright mb-0 px-3 py-4 border-top border-2 w-100 text-center'>Cahan George &copy;2021</div>
         </footer>
     )
 }
